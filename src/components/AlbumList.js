c4c29a8f69ecbbf78742b36e53c66c383822971e
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-21 12:11:55 -0800

    Completed Discussion Qs

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,9 +3,18 @@ import AlbumCard from "./AlbumCard";
 import { albums } from "../data/albums";
 
 function AlbumList() {
+  console.log(albums)
+  const albumComponents = albums.map(function(albumObj) {
+    return <AlbumCard 
+      name = {albumObj.name}
+      image = {albumObj.image}
+      genre = {albumObj.genre}
+    />
+  })
   return (
     <section className="albums">
-      <AlbumCard
+      {albumComponents}
+      {/* <AlbumCard
         name="Classic Tuna Cans Being Opened"
         image="https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg"
         genre="Cats"
@@ -24,7 +33,7 @@ function AlbumList() {
         name="Aloha K.K."
         image="https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742"
         genre="Dogs"
-      />
+      /> */}
     </section>
   );
 }
